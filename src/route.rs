52d443ef44c020ed6056c848107a20f5a74f2739
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-01 21:28:21 +0200

    Refactor route.rs to add module retrieval function by ID with language filtering

--- src/route.rs
+++ src/route.rs
@@ -1,19 +1,25 @@
-use crate::module::get_modules;
+use crate::module::{get_module, get_modules};
 use actix_web::{get, web, HttpResponse, Responder};
 use serde::Deserialize;
 use serde_json::json;
 
 #[derive(Deserialize)]
 pub struct QueryParams {
-    lang: Option<String>,
-    year: Option<String>,
+    pub lang: Option<String>,
+    pub year: Option<String>,
 }
 
 #[get("/")]
 async fn index(query: web::Query<QueryParams>) -> impl Responder {
     let modules = match get_modules(&query.lang, &query.year).await {
         Ok(modules) => modules,
-        Err(_) => json!({ "error": "Error fetching modules" }),
+        Err(err) => {
+            eprintln!("Error fetching module: {:?}", err);
+
+            return HttpResponse::InternalServerError()
+                .content_type("application/json")
+                .json(json!({ "error": "Error fetching module" }));
+        }
     };
 
     HttpResponse::Ok()
@@ -21,6 +27,28 @@ async fn index(query: web::Query<QueryParams>) -> impl Responder {
         .json(modules)
 }
 
+#[get("/{id}")]
+async fn get_module_by_id(id: web::Path<String>, query: web::Query<QueryParams>) -> impl Responder {
+    let module = match get_module(&id.into_inner(), &query.lang).await {
+        Ok(module) => module,
+        Err(err) => {
+            if err.to_string() == "Module not found" {
+                return HttpResponse::NotFound()
+                    .content_type("application/json")
+                    .json(json!({ "error": "Module not found" }));
+            }
+
+            return HttpResponse::InternalServerError()
+                .content_type("application/json")
+                .json(json!({ "error": "Error fetching module" }));
+        }
+    };
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(module)
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(index);
+    cfg.service(index).service(get_module_by_id);
 }
