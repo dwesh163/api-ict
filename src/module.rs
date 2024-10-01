aac007436bb6cc214192bdc57ddb4e6830dda32f
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-01 21:28:17 +0200

    Refactor module.rs to add module retrieval function with language and year filtering

--- src/module.rs
+++ src/module.rs
@@ -3,8 +3,9 @@ use regex::Regex;
 use reqwest;
 use serde::Deserialize;
 use serde_json::{json, Value};
+use std::error::Error;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct ApiResponse {
     value: Vec<Value>,
 }
@@ -98,4 +99,79 @@ pub async fn get_modules(
         .collect();
 
     Ok(json!(filtered_modules))
+}
+
+pub async fn get_module(id: &str, lang: &Option<String>) -> Result<Value, Box<dyn Error>> {
+    let token = auth::get_token().await?;
+    let client = reqwest::Client::new();
+
+    let url = format!(
+        "https://ictbb.crm17.dynamics.com/api/data/v9.1/beembk_handlungsnotwendigeskenntnises?$filter=startswith(beembk_hanoknr,'{}')",
+        id
+    );
+
+    let res = client.get(&url).bearer_auth(token).send().await?;
+
+    // Deserialize the response
+    let api_response: ApiResponse = res.json().await?;
+
+    // Check if the module exists
+    if api_response.value.is_empty() {
+        return Err("Module not found".into());
+    }
+
+    let module = &api_response.value[0]; // Get the first module
+
+    let re = Regex::new(r"^\d+").unwrap();
+    let lang = lang.as_deref().unwrap_or("de");
+
+    let level_name = module["beembk_levelname"].as_str().unwrap_or("");
+
+    let module_year = re
+        .find(level_name)
+        .and_then(|m| m.as_str().parse::<i64>().ok())
+        .unwrap_or_default();
+
+    let (type_key, title_key, description_key) = match lang {
+        "de" => (
+            "beembk_lernortname",
+            "beembk_modultitel",
+            "beembk_objektbeschreibung",
+        ),
+        "fr" => (
+            "beembk_lernortname_fr",
+            "beembk_modultitel_fr",
+            "beembk_objektbeschreibung_fr",
+        ),
+        "it" => (
+            "beembk_lernortname_it",
+            "beembk_modultitel_it",
+            "beembk_objektbeschreibung_it",
+        ),
+        _ => return Err("Unsupported language".into()),
+    };
+
+    let number = module["beembk_modulnummer"]
+        .as_str()
+        .unwrap_or("")
+        .parse::<i64>()
+        .unwrap_or_default();
+
+    let name = module[title_key].as_str().unwrap_or("").to_string();
+    let version = module["beembk_version"].as_i64().unwrap_or_default();
+    let last_modified = module["modifiedon"].as_str().unwrap_or("").to_string();
+    let creation_date = module["createdon"].as_str().unwrap_or("").to_string();
+    let r#type = module[type_key].as_str().unwrap_or("").to_string();
+    let description = module[description_key].as_str().unwrap_or("").to_string();
+
+    Ok(json!({
+        "number": number,
+        "description": description,
+        "name": name,
+        "year": module_year,
+        "version": version,
+        "last_modified": last_modified,
+        "creation_date": creation_date,
+        "type": r#type,
+    }))
 }
