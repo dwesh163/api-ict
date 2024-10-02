faddf0a58b711bc5d92f4c0ab83f846176083902
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-02 11:44:11 +0200

    Refactor main.rs to improve code structure and add server startup message

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use api_ict::route;
 use actix_web::{App, HttpServer};
+use api_ict::route;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .configure(route::config)
-    })
-    .bind(("127.0.0.1", 8000))?
-    .run()
-    .await
+    let server =
+        HttpServer::new(|| App::new().configure(route::config)).bind(("127.0.0.1", 8000))?;
+
+    println!("Server is running at port : 8000");
+
+    server.run().await
 }
