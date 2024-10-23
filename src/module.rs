564517a72df1a04c76a2e2ecaae61719cf740e28
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-23 12:34:07 +0200

    Refactor module.rs to update API documentation retrieval function

--- src/module.rs
+++ src/module.rs
@@ -12,100 +12,38 @@ struct ApiResponse {
 
 pub async fn get_documentation() -> Result<Value, Box<dyn std::error::Error>> {
     let documentation = json!({
-        "API-ICT": {
-            "description": "This API allows you to list all ICT modules.",
-            "endpoints": [
-                {
-                    "url": "/",
-                    "method": "GET",
-                    "description": "Retrieve the API documentation for available routes and usage."
+        "description": "This API allows you to list all ICT modules.",
+        "endpoints": [
+            {
+                "url": "/",
+                "method": "GET",
+                "description": "Retrieve the API documentation for available routes and usage."
+            },
+            {
+                "url": "/jobs",
+                "method": "GET",
+                "description": "Retrieve a list of Job with their IDs.",
+            },
+            {
+                "url": "/modules",
+                "method": "GET",
+                "description": "Get a list of modules filtered by job ID.",
+                "parameters": {
+                    "jobId": "optional, Module group ID (e.g., 89494)",
+                    "lang": "optional, Response language (FR, DE, IT)",
+                    "year": "optional, Year of the modules (e.g., 2024)"
                 },
-                {
-                    "url": "/jobs",
-                    "method": "GET",
-                    "description": "Retrieve a list of Job with their IDs.",
-                    "response": [
-                        {
-                            "id": 89494,
-                            "name": "Job A"
-                        },
-                        {
-                            "id": 83947,
-                            "name": "Job B"
-                        }
-                    ]
+            },
+            {
+                "url": "/modules/{moduleId}",
+                "method": "GET",
+                "description": "Retrieve detailed information for a specific module by Number.",
+                "parameters": {
+                    "lang": "optional, Response language (FR, DE, IT)",
+                    "year": "optional, Year of the modules (e.g., 2024)"
                 },
-                {
-                    "url": "/modules",
-                    "method": "GET",
-                    "description": "Get a list of modules filtered by job ID.",
-                    "parameters": {
-                        "jobId": "optional, Module group ID (e.g., 89494)",
-                        "lang": "optional, Response language (FR, DE, IT)",
-                        "year": "optional, Year of the modules (e.g., 2024)"
-                    },
-                    "response": [
-                        {
-                            "creation_date": "2021-09-28T05:33:38Z",
-                            "description": "Planung der Installation eines neuen lokalen Netzwerks ohne zentrale Benutzerverwaltung mit bis zu 10 Arbeitsplätzen und Internetanschluss, das Computer und Drucker in verschiedenen Räumen des gleichen Gebäudes miteinander verbindet. Installation der Netzwerkkomponenten ab der LAN-Steckdose (Computer, lokaler Drucker).",
-                            "last_modified": "2024-07-22T11:32:32Z",
-                            "name": "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
-                            "number": 117,
-                            "type": "Berufsfachschule",
-                            "version": 4,
-                            "year": 1
-                        },
-                        {
-                            "creation_date": "2021-02-11T14:03:15Z",
-                            "description": "Aufträge im eigenen Berufsumfeld mit definierten Zielen und Ergebnissen.",
-                            "last_modified": "2024-07-22T13:29:08Z",
-                            "name": "Aufträge im eigenen Berufsumfeld selbstständig durchführen",
-                            "number": 431,
-                            "type": "Berufsfachschule",
-                            "version": 2,
-                            "year": 1
-                        }
-                    ]
-                },
-                {
-                    "url": "/modules/{moduleNumber}",
-                    "method": "GET",
-                    "description": "Retrieve detailed information for a specific module by Number.",
-                    "parameters": {
-                        "lang": "optional, Response language (FR, DE, IT)",
-                        "year": "optional, Year of the modules (e.g., 2024)"
-                    },
-                    "response": {
-                        "competence": "Liest Daten zu Analyse ein, validiert diese und erstellt mit Hilfe geeigneter Metriken, Grenzwerte und Indikatoren Reports und graphische Darstellungen von Monitoring Daten. Gibt Daten anonymisiert an Dritte weiter.",
-                        "creation_date": "2021-02-26T07:44:10Z",
-                        "description": "Unbearbeitete Monitoring Daten (z.B. Logdaten)",
-                        "last_modified": "2023-02-20T07:14:33Z",
-                        "name": "Daten mit Tools analysieren und darstellen",
-                        "number": 110,
-                        "objectives": [
-                            {
-                                "details": [
-                                    "Kennt Indikatoren, welche für die Bildung von Reports benötigt werden (z.B. Zeit, Fehlerhäufigkeit). 110.1.1",
-                                    "Kennt den Aufbau eines Reports. 110.1.2"
-                                ],
-                                "name": "Gewinnt aus vorhandenen Daten Indikatoren für Bildung von Reports."
-                            },
-                            {
-                                "details": [
-                                    "Kennt die Definition eines Grenzwertes für Alerts. 110.2.1",
-                                    "Kennt Methoden, um Alerts zu erstellen. 110.2.2"
-                                ],
-                                "name": "Definiert Grenzwerte für Alerts."
-                            }
-                        ],
-                        "pdf": "https://www.modulbaukasten.ch/Module/110_1_Daten%20mit%20Tools%20analysieren%20und%20darstellen.pdf",
-                        "type": "",
-                        "version": 1,
-                        "year": 0
-                    }
-                }
-            ]
-        }
+            }
+        ]
     });
 
     Ok(documentation)
