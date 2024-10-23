5a035dc1ad6b6a4c830bdb7c301f3749728322ce
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-10-23 12:34:15 +0200

    Refactor route.rs to update API documentation retrieval and add new endpoints

--- src/route.rs
+++ src/route.rs
@@ -1,4 +1,4 @@
-use crate::module::{get_module, get_modules};
+use crate::module::{get_documentation, get_module, get_modules};
 use actix_web::{get, web, HttpResponse, Responder};
 use serde::Deserialize;
 use serde_json::json;
@@ -10,7 +10,30 @@ pub struct QueryParams {
 }
 
 #[get("/")]
-async fn index(query: web::Query<QueryParams>) -> impl Responder {
+async fn index() -> impl Responder {
+    match get_documentation().await {
+        Ok(documentation) => HttpResponse::Ok()
+            .content_type("application/json")
+            .json(documentation),
+        Err(err) => {
+            eprintln!("Error fetching documentation: {:?}", err);
+
+            return HttpResponse::InternalServerError()
+                .content_type("application/json")
+                .json(json!({ "error": "Error fetching documentation" }));
+        }
+    }
+}
+
+#[get("/jobs")]
+async fn job() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(json!({ "message": "Job started" }))
+}
+
+#[get("/modules")]
+async fn modules(query: web::Query<QueryParams>) -> impl Responder {
     let modules = match get_modules(&query.lang, &query.year).await {
         Ok(modules) => modules,
         Err(err) => {
@@ -28,7 +51,7 @@ async fn index(query: web::Query<QueryParams>) -> impl Responder {
 }
 
 #[get("/module/{id}")]
-async fn get_module_by_id(id: web::Path<String>, query: web::Query<QueryParams>) -> impl Responder {
+async fn module_by_id(id: web::Path<String>, query: web::Query<QueryParams>) -> impl Responder {
     let module = match get_module(&id.into_inner(), &query.lang).await {
         Ok(module) => module,
         Err(err) => {
@@ -50,5 +73,8 @@ async fn get_module_by_id(id: web::Path<String>, query: web::Query<QueryParams>)
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(index).service(get_module_by_id);
+    cfg.service(index)
+        .service(module_by_id)
+        .service(modules)
+        .service(job);
 }
