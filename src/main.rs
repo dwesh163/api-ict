0c94250b3074f91a03f7567567fcc1c9a45bfab8
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-11-01 16:00:22 +0100

    Refactor code to dynamically bind server to specified port

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use actix_web::{App, HttpServer};
 use api_ict::route;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server = HttpServer::new(|| App::new().configure(route::config)).bind(("0.0.0.0", 8000))?;
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8000".to_string())
+        .parse()
+        .unwrap_or(8000);
 
-    println!("Server is running at port : 8000");
+    let server = HttpServer::new(|| App::new().configure(route::config)).bind(("0.0.0.0", port))?;
+
+    println!("Server is running at port: {}", port);
 
     server.run().await
 }
